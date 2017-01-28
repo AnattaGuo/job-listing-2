cd1202b47c407153fa96421e86f24862e93c7984
AnattaGuo <mengjunkwok@gmail.com>
2017-01-28 17:58:24 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
-
   namespace :admin do
-    resources :jobs
-  end
-
-  namespace :admin do
-  resources :jobs do
-    member do
-      post :publish
-      post :hide
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
     end
   end
-end
+
+  resources :jobs do
+    resources :resumes
+  end
 
   root 'jobs#index'
 end
